fc3fbe25b823263bdbedba7ac601e1ae0eaec0cd
apprakash <101494351+apprakash@users.noreply.github.com>
2023-05-30 16:51:44 +0530

    Add new button

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,8 @@
 import React, { useState } from "react"
 import Image from "next/image"
 import Link from "next/link"
+import { faPlusCircle } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import axios from "axios"
 import useSWR from "swr"
 
@@ -36,37 +38,50 @@ export default function IndexPage() {
       {data?.length === 0 ? (
         <EmptyDashboard newlyAdded={newlyAdded} />
       ) : (
-        <div className="container mx-auto p-4 md:px-20 flex justify-center">
-          <div className="grid grid-cols-1 gap-4 space-y-2 md:grid-cols-2 lg:grid-cols-3">
-            {data?.map((item: video, index: number) => (
-              <div className="card">
-                <Link href={`/transcript/${item.id}`}>
-                  <Image
-                    src={item.thumbnail_url}
-                    alt={item.title}
-                    width={400}
-                    height={200}
-                    className={cn(
-                      " object-cover transition-all hover:scale-105"
-                    )}
-                  />
-                </Link>
-
-                <div className="card-body">
-                  <Link href={`/transcript/${item.id}`}>
-                    <h5 className="mb-2 text-xl font-semibold">{item.title}</h5>
-                  </Link>
-                  <Link
-                    href={`/transcript/${item.id}`}
-                    className={buttonVariants({ variant: "default" })}
-                  >
-                    {item.transcription_status}
-                  </Link>
-                </div>
-              </div>
-            ))}
+        <>
+          <div>
+            <button className="font-semibold py-2 px-4 mt-2 rounded-md float-right mr-40 bg-primary text-primary-foreground hover:bg-primary/90">
+              <FontAwesomeIcon
+                icon={faPlusCircle}
+                className="mr-2 bg-primary"
+              />{" "}
+              Add New
+            </button>
           </div>
-        </div>
+          <div className="container mx-auto p-4 md:px-20 flex justify-center">
+            <div className="grid grid-cols-1 gap-4 space-y-2 md:grid-cols-2 lg:grid-cols-3">
+              {data?.map((item: video, index: number) => (
+                <div className="card">
+                  <Link href={`/transcript/${item.id}`}>
+                    <Image
+                      src={item.thumbnail_url}
+                      alt={item.title}
+                      width={400}
+                      height={200}
+                      className={cn(
+                        " object-cover transition-all hover:scale-105"
+                      )}
+                    />
+                  </Link>
+
+                  <div className="card-body">
+                    <Link href={`/transcript/${item.id}`}>
+                      <h5 className="mb-2 text-xl font-semibold">
+                        {item.title}
+                      </h5>
+                    </Link>
+                    <Link
+                      href={`/transcript/${item.id}`}
+                      className={buttonVariants({ variant: "default" })}
+                    >
+                      {item.transcription_status}
+                    </Link>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
       )}
     </div>
   )
